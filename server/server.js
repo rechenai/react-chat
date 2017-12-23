0dbdd51ae6bfcb3e661303ce6b45841e3fb7f98c
HSY <huangshan_yu@yeah.net>
2017-12-23 10:21:36 +0800

    10.2success

--- server/server.js
+++ server/server.js
@@ -3,12 +3,19 @@ const userRouter = require('./user')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
+
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', function(socket) {
+  console.log('connect success')
+})
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user', userRouter)
 
-app.listen(9093, function () {
+server.listen(9093, function () {
   console.log('start ...')
 })
--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import io from 'socket.io-client'
 
 class Chat extends React.Component{
+  componentDidMount () {
+    const socket = io('ws://localhost:9093')
+  }
+
   render () {
     return <h2>{this.props.match.params.user}</h2>
   }
